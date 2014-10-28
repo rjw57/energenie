1ed92f43a0dc6587d72b1e37b8046c0e324376cc
Ben Nuttall <ben@bennuttall.com>
2014-10-28 17:50:34 +0000

    Remove demo files, update README

--- energenie-demo.py
+++
@@ -1,9 +0,0 @@
-from energenie import switch_on, switch_off
-
-print("Sockets 1-4 or 0 for all")
-
-while True:
-    socket = int(raw_input('Turn socket on: '))
-    switch_on(socket)
-    socket = int(raw_input("Turn socket off: "))
-    switch_off(socket)
