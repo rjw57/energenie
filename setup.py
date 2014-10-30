40183d3ef5b49626e62e2bfd830ad54cfa138df9
Ben Nuttall <ben@bennuttall.com>
2014-10-30 17:31:23 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "energenie",
+    version = "0.1.0",
+    author = "Ben Nuttall",
+    author_email = "ben@raspberrypi.org",
+    description = "Python module to control the Energenie add-on board for the Raspberry Pi used for remotely turning power sockets on and off.",
+    license = "BSD",
+    keywords = [
+        "energenie",
+        "raspberry pi",
+    ]
+    url = "https://github.com/bennuttall/energenie",
+    packages = [
+        "energenie",
+    ],
+    long_description = read('README.md'),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Home Automation",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
