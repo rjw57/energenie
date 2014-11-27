ea75ec922e967bced7912b7c7f2800ab1e9a7dfb
Ben Nuttall <ben@bennuttall.com>
2014-11-27 22:18:51 +0000

    Add __version__ to module

--- energenie/__init__.py
+++ energenie/__init__.py
@@ -1,2 +1,5 @@
 from __future__ import absolute_import
 from .energenie import switch_on, switch_off
+
+
+__version__ = '0.2.3'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="energenie",
-    version="0.2.2",
+    version="0.2.3",
     author="Ben Nuttall",
     author_email="ben@raspberrypi.org",
     description="Remotely control power sockets from the Raspberry Pi",
